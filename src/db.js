8740d763047b19829df4c6a2c3b662c470890bbb
githuberuser7776 <samuelwangus@gmail.com>
2017-12-08 13:50:20 -0500

    final Changes

--- src/db.js
+++ src/db.js
@@ -14,11 +14,12 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
  const path = require('path');
  const fn = path.join(__dirname, 'config.json');
  const data = fs.readFileSync(fn);
-console.log("hello");
+ console.log("hello");
 
  // our configuration file will be in json, so parse it and set the
  // conenction string appropriately!
  const conf = JSON.parse(data);
+
  dbconf = conf.dbconf;
  console.log(dbconf);
 } else {
